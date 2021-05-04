f3d4332d9b0aa09b57e80c55fc999e83de15c36f
Diony Ruaro <diony.ruaro@rduphold.com.br>
2021-05-04 13:41:47 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/DionyRuaro" target="_blank" rel="noreferrer">Diony Ruaro</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento de Estudo RDUphold: <a href="https://www.instagram.com/rduphold/" target="_blank" rel="noreferrer">@rduphold</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
